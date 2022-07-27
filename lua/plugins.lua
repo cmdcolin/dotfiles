802509375d9a7441c72f8ce874cf07fc30b6eee2
Colin <colin.diesh@gmail.com>
2022-07-26 19:07:13 -0600

    Switch to lsp-zero

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,18 +4,9 @@ return require('packer').startup(function(use)
   use 'kyazdani42/nvim-web-devicons'
   use 'tjdevries/colorbuddy.vim'
   use 'tpope/vim-vinegar'
-  use 'tpope/vim-commentary'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
   use 'tjdevries/gruvbuddy.nvim'
-  use 'williamboman/nvim-lsp-installer'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
   use 'mhinz/vim-startify'
   use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
@@ -31,5 +22,25 @@ return require('packer').startup(function(use)
     config = function()
       require('Comment').setup()
     end,
+  }
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/nvim-lsp-installer' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    },
   }
 end)
