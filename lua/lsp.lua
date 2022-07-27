ca21692dff428e854aef23bfd4604315690a9cd5
Colin <colin.diesh@gmail.com>
2022-07-26 19:07:13 -0600

    Switch to mason.nvim

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,4 +1,6 @@
-local lsp = require 'lsp-zero'
+require('mason').setup()
+require('mason-lspconfig').setup()
 
+local lsp = require 'lsp-zero'
 lsp.preset 'recommended'
 lsp.setup()
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,6 +6,7 @@ return require('packer').startup(function(use)
   use 'tpope/vim-vinegar'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
+  use 'tpope/vim-commentary'
   use 'tjdevries/gruvbuddy.nvim'
   use 'mhinz/vim-startify'
   use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
@@ -17,11 +18,27 @@ return require('packer').startup(function(use)
       require('nvim-surround').setup()
     end,
   }
+
   use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end,
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    },
   }
   use {
     'VonHeikemen/lsp-zero.nvim',
