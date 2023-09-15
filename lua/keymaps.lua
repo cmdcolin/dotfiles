4367aade50dd05938898203b0b11edc09291c2b1
Colin <colin.diesh@gmail.com>
2023-09-14 22:53:05 -0400

    Simplify stuff i didn't understand out of keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,13 +1,9 @@
-local map = vim.api.nvim_set_keymap
 local builtin = require 'telescope.builtin'
-local default_opts = { noremap = true, silent = true }
-map('i', 'jj', '<Esc>', { noremap = true })
-map('i', 'kk', '<C-\\><C-o>:w<CR>', { noremap = true })
-map('n', 'kk', '<cmd>:w<CR>', default_opts)
-map('n', '00', '<cmd>q<cr>', default_opts)
-map('n', '<leader>gg', '<cmd>Telescope git_files<cr>', default_opts)
-map('n', '<leader>o', 'o<Esc>', default_opts)
-map('n', '<leader>O', 'O<Esc>', default_opts)
-map('n', '<leader>p', '"_dP', default_opts)
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'kk', '<C-\\><C-o>:w<CR>')
+vim.keymap.set('n', 'kk', '<cmd>:w<CR>')
+vim.keymap.set('n', '00', '<cmd>q<cr>')
+vim.keymap.set('n', '<leader>p', '"_dP')
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.live_grep)
+vim.keymap.set('n', '<leader>gg', builtin.find_files)
